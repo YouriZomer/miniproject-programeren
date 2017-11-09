19709b2e983199d94e6da5394f6c41f94dca10b9
YouriZomer <31735527+YouriZomer@users.noreply.github.com>
2017-11-09 11:29:13 +0100

    Add files via upload

--- miniproject-programeren/Code.py
+++ miniproject-programeren/Code.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter as Tk
+import requests
 import xmltodict
 from tkinter.messagebox import showinfo
 
@@ -21,7 +22,7 @@ def buitenland():
 def ReisInfo(): #connectie met api
     gebruiker = (user, api)
     url = 'http://webservices.ns.nl/ns-api-avt?station=' + str(entry.get())
-    terugwaarde = url, auth=gebruiker
+    terugwaarde = requests.get(url, auth=gebruiker)
     info = terugwaarde
     return info
 
@@ -52,18 +53,15 @@ def ReisTijden():#voor het geven van de reisinformatie
     return tijden
 
 def toonhoofdscherm():
-    stadinfo.pack_forget()
     infoscherm.pack_forget()
     hoofdscherm.pack(fill='both', expand=True)
 
+
 def tooninfoscherm():#hier zitten code bij voor het background image
     global informatiescherm
-    stadinfo.pack_forget()
 
     actijden = ReisTijden()
-    if actijden == "":
-        toonstadinfo()
-    else:
+    if actijden != "":
         informatiescherm= Frame(master=root)
         infoscherm.pack()
         BGI3 = Tk.Label(master=informatiescherm, image=BGI2)
@@ -72,60 +70,46 @@ def tooninfoscherm():#hier zitten code bij voor het background image
         label2.pack(pady=5)
         terugknop2 = Button(master=informatiescherm, text='terug', background='gold', font=('calibri', 16, 'bold'))
         terugknop2.pack(pady=5)
-        label3 = Label(master=informatiescherm, background='gold', foreground='blue', font=('calibri', 9, 'bold'), text=actueletijden)
+        label3 = Label(master=informatiescherm, background='gold', foreground='blue', font=('calibri', 9, 'bold'), text=actijden)
         label3.pack(pady=(0,15))
 
         infoscherm.pack(fill='both', expand=True)
         ReisInfo()
 
-def toonstadinfo():
-    hoofdscherm.pack_forget()
-    infoscherm.pack_forget()
-    stadinfo.pack(fill='both', expand=True)
-
-
 root = Tk.Tk()
 
 root.resizable(width=False, height=False)
-root.geometry("784x590+300+50")
+root.geometry("784x590")
 BGI = Tk.PhotoImage(file='automaat v2.png')
-BGI2 = Tk.PhotoImage(file='geel.png')
+BGI2 = Tk.PhotoImage(file='automaat v2.png')
 BGL = Tk.Label(master=root, image=BGI)
 BGL.place(x=0, y=0, width=784, height=590)
 
 hoofdscherm = Frame(master=root)
 hoofdscherm.pack(expand=True)
 
-button1 = Button(master=root, text='Ik heb geen OV-chipkaart', background='blue', foreground='white', command=GeenOV)
-button1.place(x=300, y=500)
+button1 = Button(master=root, text='Ik heb geen OV-chipkaart',font=('Franklin Gothic Medium', 10, 'bold'), background='blue', foreground='white', command=GeenOV)
+button1.place(x=275, y=455, width=217, height=80)
 
-button2 = Button(master=root, text='Ik wil naar het buitenland', background='blue', foreground='white', command=buitenland)
-button2.place(x=523, y=500)
+button2 = Button(master=root, text='Ik wil naar het buitenland',font=('Franklin Gothic Medium', 10, 'bold'), background='blue', foreground='white', command=buitenland)
+button2.place(x=510, y=455, width=217, height=80)
 
-button3 = Button(master=root, text='Papieren kaartjes', background='blue', foreground='white', command=papier)
-button3.place(x=100, y=500)
+button3 = Button(master=root, text='Papieren kaartjes',font=('Franklin Gothic Medium', 10, 'bold'), background='blue', foreground='white', command=papier)
+button3.place(x=40, y=455, width=217, height=80)
 
-button4 = Button(master=root, text='Actuele reisinformatie', background='blue', foreground='white', command=toonstadinfo)
-button4.place(x=523, y=50)
+button4 = Button(master=root, text='Actuele reisinformatie',font=('Franklin Gothic Medium', 10, 'bold'), background='blue', foreground='white', command=toonhoofdscherm)
+button4.place(x=455, y=8, width=273, height=80)
 
-stadinfo = Frame(master=root)
-stadinfo.pack()
-hoofdscherm.pack_forget()
+label = Label(master=root, text='voer stations naam in', foreground='blue', background='gold', font=('Calibri', 16, 'bold'))
+label.place(x=500, y=500)
 
-BGI2=Tk.PhotoImage(file='automaat v2.png')
-BGL2=Tk.Label(master=stadinfo, image=BGI2)
-BGL2.place(x=0, y=0, width=784, height=590)
-
-label = Label(master=stadinfo, text='voer stations naam in', foreground='blue', background='gold', font=('Calibri', 16, 'bold'))
-label.pack(pady=5)
-
-entry = Entry(master=stadinfo)
+entry = Entry(master=root)
 entry.pack(pady=5)
 
-button5 = Button(master=stadinfo, text='check actuele reistijden', background='blue', foreground='white', command=tooninfoscherm)
+button5 = Button(master=root, text='check actuele reistijden', background='blue', foreground='white', command=tooninfoscherm)
 button5.pack(pady=5)
 
-terugknop = Button(master=stadinfo, text='Terug', background='blue', foreground='white', command=toonhoofdscherm)
+terugknop = Button(master=root, text='Terug', background='blue', foreground='white', command=toonhoofdscherm)
 terugknop.pack(pady=5)
 
 infoscherm = Frame(master=root)
